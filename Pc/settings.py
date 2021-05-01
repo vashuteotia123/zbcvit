9c47dcedc9eaee9acb10486d257beaa28e728cbe
Vishal Teotia <63050475+vashuteotia123@users.noreply.github.com>
2021-05-01 14:16:01 +0530

    Update settings.py

--- Pc/settings.py
+++ Pc/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL = '/media/'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'zbcvitc@gmail.com'
-EMAIL_HOST_PASSWORD = 'srnerehxjzxxhbwn'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
